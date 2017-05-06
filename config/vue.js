0cb24dc613d59d0443cb7b48b83db02ff67233c6
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-05-06 16:49:15 +0600

    delete assets

--- config/vue.js
+++ config/vue.js
@@ -1,7 +1,22 @@
 module.exports = {
-  // compile all found in src directory 
+  options: {
+    quoteChar: "'",
+    htmlmin: {
+      collapseBooleanAttributes: true,
+      collapseWhitespace: true,
+      removeAttributeQuotes: false,
+      removeComments: true,
+      removeEmptyAttributes: true,
+      removeRedundantAttributes: false,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true
+    },
+    indentString: '  ',
+    process: true
+  },
+// compile all found in src directory 
   task: {
+    src: 'template/vue/',
     dest: 'template/javascript/modules/build_vue.js',
-    src: 'template/vue/main.js',
   }
 };
