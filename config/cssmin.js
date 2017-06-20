d11546dbeadca765dc0df8f1d7ac836358ddf54f
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-06-20 13:29:59 +0600

    old node_modules working!

--- config/cssmin.js
+++ config/cssmin.js
@@ -16,7 +16,7 @@ module.exports = {
     files: {
       'assets/css/libs.min.css' : [
         // bower:css
-        "libs/bootstrap/dist/css/bootstrap.css",
+        "libs/bootstrap/dist/css/bootstrap.min.css",
         "libs/slick-carousel/slick/slick.css",
         "libs/animate.css/animate.css",
         // endbower
