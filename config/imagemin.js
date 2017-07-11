a992493a4b11cbfd2f032bb125da47556862aa4e
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-07-11 22:21:45 +0500

    use grunt-contrib-imagemin 2.0.1

--- config/imagemin.js
+++ config/imagemin.js
@@ -2,7 +2,7 @@ module.exports = {
   dynamic: {
     files: [{
       expand: true,
-      cwd: 'template/media/img/',
+      cwd: 'template/media/img',
       src: ['**/*.{png,jpg,gif}', '!sprite/**/*'],
       dest: 'assets/img/'
     }]
