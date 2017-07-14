f47719692207cab30cfbc60548d9acfb322281ab
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-07-14 15:17:59 +0500

    add spec php tests

---
+++ spec/indexTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace TestNamespace;
+
+use PHPUnit\Framework\TestCase;
+
+class TestCaseClass extends TestCase
+{
+  /**
+   * @dataProvider provider
+   */
+  public function testMethod($data)
+  {
+      $this->assertTrue($data);
+  }
+
+  public function provider()
+  {
+    return [
+      'my named data' => [true],
+      'my data'       => [true]
+    ];
+  }
+}
+
+echo 'Current version PHP: ' . phpversion();
+foreach ($_ENV as $var) {
+  echo $var;
+}
+/* phpinfo(); */
+?>
