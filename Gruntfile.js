edad9ca1b0a8ed39d0c8d54037f265c48a40df35
mifrill <alexei.mifrill.strizhak@gmail.com>
2017-12-21 22:46:01 +0500

    bootstrap hash

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,17 @@
 
 var path = require('path')
 
+/* ------ Bootstrap bower hack ----start */
+const editJsonFile = require("edit-json-file")
+let file = editJsonFile(`${__dirname}/libs/bootstrap/bower.json`)
+
+file.data.main[0] = "dist/css/bootstrap.css"
+file.set("main", file.data.main)
+file.save()
+
+console.log("paths for bower auto include:", file.data.main)
+/* ------ Bootstrap bower hack ------end */
+
 module.exports = function (grunt) {
   require('load-grunt-config')(grunt, {
     configPath: path.join(process.cwd(), 'config')
